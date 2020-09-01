077e934d6cf667ee6a612e320732460340b8ee6a
idj001 <61567197+idj001@users.noreply.github.com>
2020-09-01 15:09:57 -0400

    Create hangman.css

---
+++ hangman.css
@@ -0,0 +1,84 @@
+<?php
+class Page {
+  var $Title;
+  var $Keywords;
+  var $Content;
+  var $score;
+  var $tries;
+  var $left;
+
+
+  function Display( ) {
+    echo "<HTML>\n<HEAD>\n";
+    $this->DisplayTitle( );
+    $this->DisplayKeywords( );
+    echo "\n</HEAD>\n<BODY>\n";
+    echo $this->Content;
+    echo $this->score;
+    			switch($tries){
+				case 0:
+					?> <img src="hangman_9.png"> <?php
+				break;
+				case 1:
+					?> <img src="hangman_8.png"> <?php
+				break;
+				case 2:
+					?> <img src="hangman_7.png"> <?php
+				break;
+				case 3:
+					?> <img src="hangman_6.png"> <?php
+				break;
+				case 4:
+					?> <img src="hangman_5.png"> <?php
+				break;
+				case 5:
+					?> <img src="hangman_4.png"> <?php
+				break;
+				case 6:
+					?> <img src="hangman_3.png"> <?php
+				break;
+				case 7:
+					?> <img src="hangman_2.png"> <?php
+				break;
+				case 8:
+					?> <img src="hangman_1.png"> <?php
+				break;
+				case 9:
+					?> <img src="hangman_0.png"> <?php
+				break;
+				case 10:
+					?> <img src=""> <?php
+				break;
+			}
+		
+	echo "	<br />" .$this->alpha;
+	echo "<br />".$this->left;
+}
+  function DisplayTitle( ) {
+    echo "<TITLE>" . $this->Title . "</TITLE>\n";
+  }
+
+  function DisplayKeywords( ) {
+    echo '<META NAME="keywords" CONTENT="' . $this->Keywords . '">';
+  }
+
+  function SetContent( $Data ) {
+    $this->Content = $Data;
+  }
+  function SetScore( $Data ) {
+    $this->score = $Data;
+  }
+   function SetTries( $Data ) {
+    $this->tries = $Data;
+  }
+    function SetAlpha( $Data ) {
+    $this->alpha = $Data;
+  }
+      function SetLeft( $Data ) {
+    $this->left = $Data;
+  }
+
+}
+?>
+
+
